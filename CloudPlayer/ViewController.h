c7e0eed0322977d0e11dca778c8f469e3ef6c0f9
herui03 <herui03@baidu.com>
2017-04-12 18:08:53 +0800

    Initial Commit

---
+++ CloudPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CloudPlayer
+//
+//  Created by He,Rui on 2017/4/12.
+//  Copyright © 2017年 RyonLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
